476e4fe74a5d8076366d421b14ced827cc2ed089
Paul <=>
2024-08-14 09:36:15 +0800

    Project scaffolding: Created base classes and forms

---
+++ src/main/java/com/mycompany/payrollsystem/PayrollSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.payrollsystem;
+
+/**
+ *
+ * @author Paul
+ */
+public class PayrollSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
