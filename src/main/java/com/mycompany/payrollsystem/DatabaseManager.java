306327196748478abbe75955eed7de6656419f06
Aron-Balines <balinesaron@gmail.com>
2024-08-14 13:17:36 +0800

    Add files via upload

--- src/main/java/com/mycompany/payrollsystem/DatabaseManager.java
+++ src/main/java/com/mycompany/payrollsystem/DatabaseManager.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.payrollsystem;
 
-/**
- *
- * @author Paul
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseManager {
-    
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/payroll_system";
+        String username = "root";
+        String password = "";   
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection to MySQL database established successfully.");
+            
+            Statement statement = connection.createStatement();
+            
+            ResultSet resultSet = statement.executeQuery("select * from users");
+            
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2)+" "+resultSet.getString(3));
+            }  
+            
+            connection.close();
+        } 
+        catch (ClassNotFoundException e) {
+            System.err.println("MySQL JDBC Driver not found: " + e.getMessage());
+
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+            System.err.println("SQLState: " + e.getSQLState());
+            System.err.println("VendorError: " + e.getErrorCode());
+        }
+    }
 }
